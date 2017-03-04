8c2be35e1e0c68dfed61c3f5eaaa1d43a1a44599
billypoke <ransom.pate@gmail.com>
2017-03-04 04:12:19 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# statschecker
+# EVE ESI Skill Viewer
+
+## What is this?
+
+A web app to view your skills, grouped into useful categories. See where you have good coverage, and where you need to 
+train your skills.
+
+## How does it work?
+
+Using [EVE's SSO](https://community.eveonline.com/news/dev-blogs/eve-online-sso-and-what-you-need-to-know/) 
+(Single Sign On) system and the new [ESI API](https://esi.tech.ccp.is/latest/). You are guaranteed to be going to CCP's 
+server for authentication, and you can see exactly what permissions you are granting to this application.
+
+## Installation
+
+Python 3.x is required.
+
+1. `pip install -r requirements.txt`
+2. Copy `config.conf.default` to `config.conf`
+3. Go to https://developers.eveonline.com/ and create a new application
+4. Choose a name and write a description
+5. For 'Connection Type', choose `Authentication & API Access`
+6. The required scopes are listed in `config.conf.deafult` but are `publicData` & `esi-skills.read_skills.v1`
+7. The Callback URL is where the SSO system will redirect your users after authentication. It should end in 
+`/statschecker/view` for this application
+8. Copy the information for your app into `config.conf`
+9. Run `python wsgi.py`
---
+++ statschecker.py
@@ -0,0 +1,117 @@
+import json
+
+import requests
+import yaml
+from flask import Flask, render_template, flash, request, redirect, url_for
+from preston.esi import Preston
+
+application = Flask(__name__)
+
+names_url = 'https://esi.tech.ccp.is/latest/universe/names/?datasource=tranquility'
+config = yaml.load(open('config.conf', 'r'))
+
+application.config['SECRET_KEY'] = config['SECRET_KEY']
+
+preston = Preston(
+    user_agent=config['EVE_OAUTH_USER_AGENT'],
+    client_id=config['EVE_OAUTH_CLIENT_ID'],
+    client_secret=config['EVE_OAUTH_SECRET'],
+    callback_url=config['EVE_OAUTH_CALLBACK'],
+    scope=config['EVE_OAUTH_SCOPE']
+)
+
+
+@application.route('/statschecker')
+@application.route('/statschecker/')
+def landing():
+    return render_template('view.html', show_crest=True, crest_url=preston.get_authorize_url())
+
+
+@application.route('/statschecker/view')
+@application.route('/statschecker/view/<refresh_token>')
+def view_pilot(refresh_token=None):
+    try:
+        if refresh_token is None:
+            # check response
+            if 'error' in request.path:
+                flash('There was an error in EVE\'s response', 'error')
+                return redirect(url_for('landing'))
+
+            try:
+                auth = preston.authenticate(request.args['code'])
+            except Exception as e:
+                print('SSO callback exception: ' + str(e))
+                flash('There was an authentication error signing you in.', 'error')
+                return redirect(url_for('landing'))
+
+            return redirect(url_for('view_pilot', refresh_token=auth.refresh_token))
+
+        else:
+            auth = preston.use_refresh_token(refresh_token)
+            pilot_info = auth.whoami()
+            pilot_name = pilot_info['CharacterName']
+            pilot_id = pilot_info['CharacterID']
+
+            flash('Logged in as: ' + pilot_name, 'success')
+            skill_groups = json.load(open('static/json/skill_groups.json', 'r'))
+
+            skills_dict = {}
+            skills_stats = {}
+
+            result = auth.characters[pilot_id].skills()
+            if result.get('error') is not None:
+                print('ESI error: ' + result.get('error'))
+                flash('ESI is not responding. Please try again, or wait a few minutes.', 'error')
+                return redirect(url_for('landing'))
+
+            skills = result['skills']
+
+            # Prepare and get names form the skill ids
+            ids_list = []
+            for skill in skills:
+                ids_list.append(skill.get('skill_id'))
+
+            try:
+                skill_names = requests.post(url=names_url, json=ids_list).json()
+
+            except Exception as e:
+                flash('There was an error in EVE\'s response', 'error')
+                print('SSO callback exception: ' + str(e))
+                return redirect(url_for('landing'))
+
+            skills_stats['Totals'] = {}
+            skills_stats['Totals']['num_skills'] = len(skills)
+            skills_stats['Totals']['total_sp'] = 0
+
+            # Parse skills into dicts for the html response
+            for group, child_skills in skill_groups.items():
+                for skill in skills:
+                    if skill['skill_id'] in child_skills:
+                        # Prevents dict errors by setting child items to {} first
+                        if group not in skills_dict:
+                            skills_dict[group] = {}
+                            skills_stats[group] = {}
+                            skills_stats[group]['skills_in_group'] = 0
+                            skills_stats[group]['sp_in_group'] = 0
+
+                        # Add skill and metadata to dicts
+                        skill_id = skill['skill_id']
+                        skill_name = next(
+                            item['name'] for item in skill_names if item['id'] == skill_id)  # stackoverflow FTW
+                        skill_level_trained = skill['current_skill_level']
+                        skills_dict[group][skill_name] = skill_level_trained
+                        skills_stats[group]['skills_in_group'] += 1
+                        skills_stats[group]['sp_in_group'] += skill['skillpoints_in_skill']
+                        skills_stats['Totals']['total_sp'] += skill['skillpoints_in_skill']
+
+    except Exception as e:
+        flash('There was an error parsing skills', 'error')
+        print('Skill Parse error: ' + str(e))
+        return redirect(url_for('landing'))
+
+    return render_template('view.html', show_crest=False, pilot_name=pilot_name, pilot_id=pilot_id, skills=skills_dict,
+                           skills_stats=skills_stats)
+
+
+if __name__ == "__main__":
+    application.run(host='0.0.0.0', debug=True, ssl_context='adhoc')
