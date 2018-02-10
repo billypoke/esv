1611135b5fbe672e826ad7b3c83d43873da14187
Ransom Pate <rpate@provantage-corp.com>
2018-02-09 19:30:18 -0500

    Switch to load skills after page load, increase speed feel

--- esv.py
+++ esv.py
@@ -49,73 +49,10 @@ def view_pilot(refresh_token=None):
             return redirect(url_for('view_pilot', refresh_token=auth.refresh_token))
 
         else:
-            t0 = time.time()
             auth = preston.use_refresh_token(refresh_token)
             pilot_info = auth.whoami()
             pilot_name = pilot_info['CharacterName']
             pilot_id = pilot_info['CharacterID']
-
-            skill_groups = json.load(open('static/json/skill_groups.json', 'r'))
-
-            skills_dict = {}
-            skills_stats = {}
-
-            result = auth.characters[pilot_id].skills()
-            if result.get('error') is not None:
-                print('ESI error: ' + result.get('error'))
-                flash('ESI is not responding. Please try again, or wait a few minutes.', 'error')
-                return redirect(url_for('landing'))
-
-            skills = result['skills']
-
-            # Prepare and get names from the skill ids
-            ids_list = []
-            for skill in skills:
-                ids_list.append(skill.get('skill_id'))
-
-            try:
-                skill_names = requests.post(url=names_url, json=ids_list).json()
-
-            except Exception as e:
-                flash('There was an error in EVE\'s response', 'error')
-                print('SSO callback exception: ' + str(e))
-                return redirect(url_for('landing'))
-
-            t1 = time.time()
-
-            network_time = t1 - t0
-
-            t0 = time.time()
-
-            skills_stats['Totals'] = {}
-            skills_stats['Totals']['num_skills'] = len(skills)
-            skills_stats['Totals']['total_sp'] = 0
-
-            # Parse skills into dicts for the html response
-            for group, child_skills in skill_groups.items():
-                for skill in skills:
-                    if skill['skill_id'] in child_skills:
-                        # Prevents dict errors by setting child items to {} first
-                        if group not in skills_dict:
-                            skills_dict[group] = {}
-                            skills_stats[group] = {}
-                            skills_stats[group]['skills_in_group'] = 0
-                            skills_stats[group]['sp_in_group'] = 0
-
-                        # Add skill and metadata to dicts
-                        skill_id = skill['skill_id']
-                        skill_name = next(
-                            item['name'] for item in skill_names if item['id'] == skill_id  # stackoverflow FTW
-                        )
-                        skill_level_trained = skill['active_skill_level']
-                        skills_dict[group][skill_name] = skill_level_trained
-                        skills_stats[group]['skills_in_group'] += 1
-                        skills_stats[group]['sp_in_group'] += skill['skillpoints_in_skill']
-                        skills_stats['Totals']['total_sp'] += skill['skillpoints_in_skill']
-
-            t1 = time.time()
-
-            parse_time = t1 - t0
 
     except Exception as e:
         flash('There was an error parsing skills', 'error')
@@ -123,7 +60,78 @@ def view_pilot(refresh_token=None):
         return redirect(url_for('landing'))
 
     return render_template('dist/view.html', show_crest=False, pilot_name=pilot_name, pilot_id=pilot_id,
-                           skills=skills_dict,skills_stats=skills_stats, network_time=network_time,
+                           base_url=config['BASE_URL'], refresh_token=refresh_token)
+
+
+@application.route('/esv/get_skills/<refresh_token>')
+def get_skills(refresh_token):
+    t0 = time.time()
+    auth = preston.use_refresh_token(refresh_token)
+    pilot_info = auth.whoami()
+    pilot_id = pilot_info['CharacterID']
+    result = auth.characters[pilot_id].skills()
+    if result.get('error') is not None:
+        print('ESI error: ' + result.get('error'))
+        flash('ESI is not responding. Please try again, or wait a few minutes.', 'error')
+        return redirect(url_for('landing'))
+
+    skills = result['skills']
+
+    # Prepare and get names from the skill ids
+    ids_list = []
+    for skill in skills:
+        ids_list.append(skill.get('skill_id'))
+
+    try:
+        skill_names = requests.post(url=names_url, json=ids_list).json()
+
+    except Exception as e:
+        flash('There was an error in EVE\'s response', 'error')
+        print('SSO callback exception: ' + str(e))
+        return redirect(url_for('landing'))
+
+    t1 = time.time()
+
+    network_time = t1 - t0
+
+    t0 = time.time()
+
+    skill_groups = json.load(open('static/json/skill_groups.json', 'r'))
+
+    skills_stats = {}
+    skills_dict = {}
+
+    skills_stats['Totals'] = {}
+    skills_stats['Totals']['num_skills'] = len(skills)
+    skills_stats['Totals']['total_sp'] = 0
+
+    # Parse skills into dicts for the html response
+    for group, child_skills in skill_groups.items():
+        for skill in skills:
+            if skill['skill_id'] in child_skills:
+                # Prevents dict errors by setting child items to {} first
+                if group not in skills_dict:
+                    skills_dict[group] = {}
+                    skills_stats[group] = {}
+                    skills_stats[group]['skills_in_group'] = 0
+                    skills_stats[group]['sp_in_group'] = 0
+
+                # Add skill and metadata to dicts
+                skill_id = skill['skill_id']
+                skill_name = next(
+                    item['name'] for item in skill_names if item['id'] == skill_id  # stackoverflow FTW
+                )
+                skill_level_trained = skill['active_skill_level']
+                skills_dict[group][skill_name] = skill_level_trained
+                skills_stats[group]['skills_in_group'] += 1
+                skills_stats[group]['sp_in_group'] += skill['skillpoints_in_skill']
+                skills_stats['Totals']['total_sp'] += skill['skillpoints_in_skill']
+
+    t1 = time.time()
+
+    parse_time = t1 - t0
+
+    return render_template('dist/ajax.html', skills=skills_dict, skills_stats=skills_stats, network_time=network_time,
                            parse_time=parse_time, base_url=config['BASE_URL'])
 
 
