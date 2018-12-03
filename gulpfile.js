384d3c020afba42a72faa2ae8c2166c193b3ce99
Ransom Pate <rpate@provantage-corp.com>
2018-12-02 21:41:45 -0500

    Fix for ajax request, skills being completed but still shown

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
 // loads various gulp modules
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-var minifyCSS = require('gulp-minify-css');
+var cleanCSS = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var htmlmin = require('gulp-htmlmin');
 
 // create task
 gulp.task('css', function(){
-    gulp.src([
+    return gulp.src([
         'static/css/bootstrap.min.css',
         'static/css/bootstrap_xl.css',
         'static/css/sticky_footer.css',
         'static/css/custom.css'
     ])
-        .pipe(minifyCSS({keepSpecialComments : 0}))
+        .pipe(cleanCSS())
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9'))
         .pipe(concat('style.min.css'))
         .pipe(gulp.dest('static/css'));
